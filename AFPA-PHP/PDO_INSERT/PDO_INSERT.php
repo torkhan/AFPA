3754f4c711771bd87d419982ba2c95a311a01b78
Marcux <marceremus@gmail.com>
2019-09-11 14:29:07 +0200

    Create PDO_INSERT.php

---
+++ AFPA-PHP/PDO_INSERT/PDO_INSERT.php
@@ -0,0 +1,33 @@
+<?php
+require 'connect.php';
+
+// créer la variable connection
+$db = connect();
+
+// la mise des valeurs reçues dans les variables
+$nomArt   = "Vélo 567";
+$descArt  = "Description du vélo 3";
+$priceArt = "9.10";
+$rateArt  = "20";
+$typeArt  = "Vélo éléctrique";
+$artFree  = 1;
+
+try {
+	// preparation de la requette
+	$req = $db->prepare("INSERT INTO articles (nom_article, description_article, price_ht_article, rate_tva_article, type_article, article_free) VALUES (:nomArt, :descArt, :priceArt, :rateArt , :typeArt, :artFree)");
+
+	// changement des paramètres et controle des champs
+
+	$req->bindParam(":nomArt", $nomArt, PDO::PARAM_STR);
+	$req->bindParam(":descArt", $descArt, PDO::PARAM_STR);
+	$req->bindParam(":priceArt", $priceArt, PDO::PARAM_STR);
+	$req->bindParam(":rateArt", $rateArt, PDO::PARAM_INT);
+	$req->bindParam(":typeArt", $typeArt, PDO::PARAM_STR);
+	$req->bindParam(":artFree", $artFree, PDO::PARAM_INT);
+
+	// execution de la requette
+	$req->execute();
+	header('Location: confirm.php');
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
