1554c26ef236f93cdb8348a0e6c52b8f491acaa7
Marcux <marceremus@gmail.com>
2019-09-11 18:37:15 +0200

    ajout eco php

---
+++ AFPA-PHP/PDO_Exercices/Products/products.php
@@ -0,0 +1,103 @@
+<?php
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<title>Document</title>
+</head>
+<body>
+<div class="container">
+	<h2>Products</h2>
+
+	<table class="table table-hover">
+		<thead class="thead-dark">
+		<tr>
+			<th scope="col">#</th>
+			<th scope="col">Description</th>
+			<th scope="col">Price</th>
+			<th scope="col">Category</th>
+			<th scope="col">Actions</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<th scope="row">1</th>
+			<td>LG P880 4X HD</td>
+			<td>336</td>
+			<td>Fashion</td>
+			<td>
+				<a href="#"><button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button></a>
+				<a href="#"><button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button></a>
+				<a href="#"><button class="btn btn-danger" type="submit"><i class="fa fa-minus-square" aria-hidden="true"></i> Supprimer</button></a>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">2</th>
+			<td>Google Nexus 4</td>
+			<td>299</td>
+			<td>Electronics</td>
+			<td>
+				<a href="#">
+<button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-danger" type="submit"><i class="fa fa-minus-square" aria-hidden="true"></i> Supprimer</button>
+				</a>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">3</th>
+			<td>Samsung Galaxy S4</td>
+			<td>600</td>
+			<td>Motors</td>
+			<td>
+				<a href="#">
+					<button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-danger" type="submit"><i class="fa fa-minus-square" aria-hidden="true"></i> Supprimer</button>
+				</a>
+			</td>
+		</tr>
+
+		<tr>
+			<th scope="row">3</th>
+			<td>Bench Shirt</td>
+			<td>29</td>
+			<td>Fashion</td>
+			<td>
+				<a href="#">
+					<button class="btn btn-primary" type="submit"><i class="fa fa-bars" aria-hidden="true"></i> Lire</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-warning" type="submit"><i class="fa fa-spinner" aria-hidden="true"></i> Modifier</button>
+				</a>
+				<a href="#">
+					<button class="btn btn-danger" type="submit"><i class="fa fa-minus-square" aria-hidden="true"></i> Supprimer</button>
+				</a>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
