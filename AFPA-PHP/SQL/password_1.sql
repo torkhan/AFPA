b4dd28eef526a43d708fcbabf79cbb43f47b8a90
YourHealth <marceremus@gmail.com>
2019-09-18 10:37:05 +0200

    correction table

--- AFPA-PHP/SQL/password_1.sql
+++ AFPA-PHP/SQL/password_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 17, 2019 at 05:29 PM
+-- Generation Time: Sep 18, 2019 at 10:35 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.7
 
@@ -13,6 +13,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `password`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typeUsers`
+--
+
+CREATE TABLE `typeUsers` (
+  `Id_typeUser` int(11) NOT NULL,
+  `typeName` varchar(50) NOT NULL,
+  `actived` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `typeUsers`
+--
+
+INSERT INTO `typeUsers` (`Id_typeUser`, `typeName`, `actived`) VALUES
+(1, 'Admin', 1),
+(2, 'Autor', 1),
+(3, 'Editor', 1);
 
 -- --------------------------------------------------------
 
@@ -41,6 +62,12 @@ INSERT INTO `users` (`id_signit`, `email`, `password`, `Id_typeUser`) VALUES
 --
 
 --
+-- Indexes for table `typeUsers`
+--
+ALTER TABLE `typeUsers`
+  ADD PRIMARY KEY (`Id_typeUser`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -49,6 +76,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `typeUsers`
+--
+ALTER TABLE `typeUsers`
+  MODIFY `Id_typeUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
